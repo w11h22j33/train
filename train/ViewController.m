ca63322ff3b7bf1782b0d0bbb10ee3a485912e2c
navy.zy <navy.zy@gmail.com>
2014-03-27 06:23:34 +0800

    add storyboard

--- train/ViewController.m
+++ train/ViewController.m
@@ -1,8 +1,8 @@
 //
 //  ViewController.m
-//  train
+//  CocoapodsDemo
 //
-//  Created by wanghaijun on 14-3-26.
+//  Created by wanghaijun on 14-3-7.
 //  Copyright (c) 2014年 ___NAVY___. All rights reserved.
 //
 
@@ -10,9 +10,20 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *tfAccount;
+@property (weak, nonatomic) IBOutlet UITextField *tfPassword;
+@property (weak, nonatomic) IBOutlet UIImageView *imageVerCode;
+
+- (IBAction)actionButtonClicked:(id)sender;
+
+- (void)doInit;
+- (void)getVerCode;
+
 @end
 
 @implementation ViewController
+
+@synthesize tfAccount,tfPassword,imageVerCode;
 
 - (void)viewDidLoad
 {
@@ -24,6 +35,41 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)actionButtonClicked:(id)sender {
+    
+    NSString* account = tfAccount.text;
+    NSString* password = tfPassword.text;
+    
+    [self doInit];
+    
+    
+}
+
+- (void)doInit{
+    
+    NSString* urlString = @"https://kyfw.12306.cn/otn/";
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    [manager GET:urlString parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Headers:%@",operation.response.allHeaderFields);
+        //        NSLog(@"responseString:%@",operation.responseString);
+        NSLog(@"StatusCode:%d",operation.response.statusCode);
+        NSLog(@"Error: %@", error);
+    }];
+    
+}
+
+- (void)getVerCode{
+    
+    
+    
 }
 
 @end
